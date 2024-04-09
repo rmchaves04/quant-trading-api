fad079262fb4f50f83397851972ab7d903e09b02
Rodrigo Chaves <119129725+rmchaves04@users.noreply.github.com>
2024-04-09 10:13:10 -0300

    [feat] Create example stock route

--- api/main.py
+++ api/main.py
@@ -1,14 +1,21 @@
-from flask import Flask
+import yfinance as yf
+from flask import Flask, jsonify
 import logging
 
 api = Flask(__name__)
 
 
 @api.route('/')
-def index() :
+def index():
     return 'Hello, World!'
 
 
+@api.route('/api/stock/<string:ticker>', methods=['GET'])
+def stock(ticker):
+    stock = yf.Ticker(ticker)
+    return jsonify(stock.history(period='1y').to_dict(orient='records'))
+
+
 if __name__ == '__main__':
-    api.run(debug=True, host='0.0.0.0', port=3000)
+    api.run(host='0.0.0.0', port=3000)
 
